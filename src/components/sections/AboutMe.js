a48a93e8d788e876c21082fde25697cf92416d85
Ann Song <gmsong1978@gmail.com>
2023-06-06 13:47:36 -0400

    About

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const AboutMe = () => {
-	return <section id="about" className="about section is-medium"></section>;
+	return (
+		<section id="about" className="about section is-medium">
+			<div className="about-container container">
+				<h1 className="title">About</h1>
+			</div>
+		</section>
+	);
 };
 
 export default AboutMe;
