5464837fd507fb162eb715075bf6353b27481e32
Ann Song <gmsong1978@gmail.com>
2023-06-06 13:45:22 -0400

    About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/sections/Header";
 import Navbar from "./components/Navbar";
+import AboutMe from "./components/sections/AboutMe";
 
 const App = () => {
 	return (
 		<div className="app">
 			<Header />
 			<Navbar />
+			<AboutMe />
 		</div>
 	);
 };
---
+++ src/components/sections/AboutMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutMe = () => {
+	return <section id="about" className="about section is-medium"></section>;
+};
+
+export default AboutMe;
